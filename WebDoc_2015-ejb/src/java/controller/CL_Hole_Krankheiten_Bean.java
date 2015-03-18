092bd911e10b0e062df469e2eca7729b27aa6c10
DEKREDAV <DEKREDAV@LTBRU73645.de.eu.sew>
2015-03-18 14:08:16 +0100

    David Kretzler
    Beans

---
+++ WebDoc_2015-ejb/src/java/controller/CL_Hole_Krankheiten_Bean.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.ejb.LocalBean;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import model.CL_Bewertetes_Symptom;
+import model.CL_Symptom;
+
+/**
+ *
+ * @author DEKREDAV
+ */
+@Stateless
+@LocalBean
+public class CL_Hole_Krankheiten_Bean {
+    
+     @PersistenceContext
+    private EntityManager io_em;
+     
+     public List<CL_Krankheit_Akt_Wert> im_get_passende_Krankheiten(List<CL_Symptom> po_gewaehlte_Symptome){
+         
+         List<CL_Bewertetes_Symptom> lo_passende_Symptome = im_get_passende_Symptome(po_gewaehlte_Symptome);
+         
+         List<CL_Krankheit> lo_krankheiten = new List<>();
+         
+         for(CL_Bewertetes_Symptom lo_bewertete_symptome : lo_passende_Symptome){
+             
+             for (CL_Krankheit lo_krankheit: lo_bewertete_symptome.)
+             
+         }
+         
+         return null;
+     }
+
+    public List<CL_Bewertetes_Symptom> im_get_passende_Symptome(List<CL_Symptom> po_gewaehlte_Symptome) {
+
+        TypedQuery<CL_Bewertetes_Symptom> io_back = io_em.createNamedQuery("Bewertetes_Symptom.findBySymptomList", CL_Bewertetes_Symptom.class)
+                .setParameter("p_Symptome", po_gewaehlte_Symptome);
+
+        return io_back.getResultList();
+    }
+
+    
+}
