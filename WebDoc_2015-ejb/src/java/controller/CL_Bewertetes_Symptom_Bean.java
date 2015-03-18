1c0d77a45c4a15e0c0147a2c76a36ec3c3829207
DEKREDAV <DEKREDAV@LTBRU73645.de.eu.sew>
2015-03-18 16:03:43 +0100

    David
    18.03
    
    Beans zum Erzeugen der Datenbanksetze

--- WebDoc_2015-ejb/src/java/controller/CL_Bewertetes_Symptom_Bean.java
+++ WebDoc_2015-ejb/src/java/controller/CL_Bewertetes_Symptom_Bean.java
@@ -5,8 +5,17 @@
  */
 package controller;
 
+import java.util.ArrayList;
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.ejb.LocalBean;
+import javax.persistence.EntityManager;
+import javax.persistence.FlushModeType;
+import javax.persistence.PersistenceContext;
+import model.CL_Bewertetes_Symptom;
+import model.CL_Empfehlung;
+import model.CL_Krankheit;
+import model.CL_Symptom;
 
 /**
  *
@@ -16,6 +25,16 @@ import javax.ejb.LocalBean;
 @LocalBean
 public class CL_Bewertetes_Symptom_Bean {
 
-    // Add business logic below. (Right-click in editor and choose
-    // "Insert Code > Add Business Method")
+    @PersistenceContext
+    private EntityManager io_em;
+    
+    public CL_Bewertetes_Symptom im_create_Krankheit(CL_Symptom po_symptom, int pv_wert, List<CL_Krankheit> po_krankheiten){
+        
+        io_em.setFlushMode(FlushModeType.AUTO);
+        CL_Bewertetes_Symptom lo_bewertetes_symptom = new CL_Bewertetes_Symptom(po_symptom, pv_wert /* ,po_krankheiten*/);
+        io_em.persist(lo_bewertetes_symptom);
+        lo_bewertetes_symptom = io_em.merge(lo_bewertetes_symptom);
+        io_em.flush();
+        return lo_bewertetes_symptom;   
+    }
 }
