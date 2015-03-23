c043aed0b86a72d1facecda6ed6ca8f95d52f8cf
DEDEUTIZ <DEDEUTIZ@LTBRU73648.de.eu.sew>
2015-03-23 17:19:46 +0100

    Tizian brechnung aktueller wert und j unit test

--- WebDoc_2015-ejb/test/controller/CL_Krankheit_Akt_WertTest.java
+++ WebDoc_2015-ejb/test/controller/CL_Krankheit_Akt_WertTest.java
@@ -45,10 +45,15 @@ public class CL_Krankheit_Akt_WertTest {
 
     /**
      * Test of im_cal_akt_wert method, of class CL_Krankheit_Akt_Wert.
+     * Test auf alle getroffenen Symptome
      */
     @Test
-    public void testIm_cal_akt_wert() {
+    public void testIm_cal_akt_wert_1() {
         System.out.println("im_cal_akt_wert");
+                
+        //Erstellen der benötigten Objekte
+        
+        //Symptome
         CL_Symptom lo_symptom_1 = new CL_Symptom("Husten");
         CL_Symptom lo_symptom_2 = new CL_Symptom("Fieber");
         CL_Symptom lo_symptom_3 = new CL_Symptom("Kopfschmerzen");
@@ -56,9 +61,10 @@ public class CL_Krankheit_Akt_WertTest {
         CL_Symptom lo_symptom_7 = new CL_Symptom("laufende Nase");
         CL_Symptom lo_symptom_8 = new CL_Symptom("Gliederschmerzen");
 
-
+        //Empfehlung
         CL_Empfehlung lo_empfhelung_1 = new CL_Empfehlung("Gehen Sie zum Arzt!");
-
+        
+        //Bewertete Symptome
         //Husten
         CL_Bewertetes_Symptom lo_husten_9 = new CL_Bewertetes_Symptom(lo_symptom_1, 9);
 
@@ -67,6 +73,7 @@ public class CL_Krankheit_Akt_WertTest {
 
         //Kopfschmerzen
         CL_Bewertetes_Symptom lo_kopfschmerzen_8 = new CL_Bewertetes_Symptom(lo_symptom_3, 8);
+        
         //Halsschmerzen
         CL_Bewertetes_Symptom lo_halsschmerzen_7 = new CL_Bewertetes_Symptom(lo_symptom_4, 7);
 
@@ -77,13 +84,15 @@ public class CL_Krankheit_Akt_WertTest {
         CL_Bewertetes_Symptom lo_glieder_7 = new CL_Bewertetes_Symptom(lo_symptom_8, 7);
 
         //Grippe
-        CL_Krankheit lo_grippe = new CL_Krankheit("Hier kommt die Beschreibung", lo_empfhelung_1, 120);
+        CL_Krankheit lo_grippe = new CL_Krankheit("Hier kommt die Beschreibung", lo_empfhelung_1, 120,"Grippe");
         lo_grippe.addSymptom(lo_husten_9);
         lo_grippe.addSymptom(lo_kopfschmerzen_8);
         lo_grippe.addSymptom(lo_halsschmerzen_7);
         lo_grippe.addSymptom(lo_nase_8);
         lo_grippe.addSymptom(lo_glieder_7);
         lo_grippe.addSymptom(lo_fieber_9);
+        
+        //Gewählte Symptome
 
         ArrayList<CL_Bewertetes_Symptom> lo_gewaehlte_symptome = new ArrayList<>();
 
@@ -105,73 +114,173 @@ public class CL_Krankheit_Akt_WertTest {
         assertEquals(expResult, result, 0.0);
        
     }
+    
+    
+     /**
+     * Test of im_cal_akt_wert method, of class CL_Krankheit_Akt_Wert.
+     * Test auf ein getroffenes Symptom
+     */
+    @Test
+    public void testIm_cal_akt_wert_2() {
+        System.out.println("im_cal_akt_wert");
+                
+        //Erstellen der benötigten Objekte
+        
+        //Symptome
+        CL_Symptom lo_symptom_1 = new CL_Symptom("Husten");
+        CL_Symptom lo_symptom_2 = new CL_Symptom("Fieber");
+        CL_Symptom lo_symptom_3 = new CL_Symptom("Kopfschmerzen");
+        CL_Symptom lo_symptom_4 = new CL_Symptom("Halsschmerzen");
+        CL_Symptom lo_symptom_7 = new CL_Symptom("laufende Nase");
+        CL_Symptom lo_symptom_8 = new CL_Symptom("Gliederschmerzen");
 
-//    /**
-//     * Test of getIo_krankheit method, of class CL_Krankheit_Akt_Wert.
-//     */
-//    @Test
-//    public void testGetIo_krankheit() {
-//        System.out.println("getIo_krankheit");
-//        CL_Krankheit_Akt_Wert instance = null;
-//        CL_Krankheit expResult = null;
-//        CL_Krankheit result = instance.getIo_krankheit();
-//        assertEquals(expResult, result);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
-//
-//    /**
-//     * Test of getIv_akt_wert method, of class CL_Krankheit_Akt_Wert.
-//     */
-//    @Test
-//    public void testGetIv_akt_wert() {
-//        System.out.println("getIv_akt_wert");
-//        CL_Krankheit_Akt_Wert instance = null;
-//        double expResult = 0.0;
-//        double result = instance.getIv_akt_wert();
-//        assertEquals(expResult, result, 0.0);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
-//
-//    /**
-//     * Test of setIo_krankheit method, of class CL_Krankheit_Akt_Wert.
-//     */
-//    @Test
-//    public void testSetIo_krankheit() {
-//        System.out.println("setIo_krankheit");
-//        CL_Krankheit io_krankheit = null;
-//        CL_Krankheit_Akt_Wert instance = null;
-//        instance.setIo_krankheit(io_krankheit);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
-//
-//    /**
-//     * Test of setIv_akt_wert method, of class CL_Krankheit_Akt_Wert.
-//     */
-//    @Test
-//    public void testSetIv_akt_wert() {
-//        System.out.println("setIv_akt_wert");
-//        double iv_akt_wert = 0.0;
-//        CL_Krankheit_Akt_Wert instance = null;
-//        instance.setIv_akt_wert(iv_akt_wert);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
-//
-//    /**
-//     * Test of compareTo method, of class CL_Krankheit_Akt_Wert.
-//     */
-//    @Test
-//    public void testCompareTo() {
-//        System.out.println("compareTo");
-//        CL_Krankheit_Akt_Wert po_krankheit_akt_wert = null;
-//        CL_Krankheit_Akt_Wert instance = null;
-//        int expResult = 0;
-//        int result = instance.compareTo(po_krankheit_akt_wert);
-//        assertEquals(expResult, result);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-//    }
+        //Empfehlung
+        CL_Empfehlung lo_empfhelung_1 = new CL_Empfehlung("Gehen Sie zum Arzt!");
+        
+        //Bewertete Symptome
+        //Husten
+        CL_Bewertetes_Symptom lo_husten_9 = new CL_Bewertetes_Symptom(lo_symptom_1, 9);
+
+        //Kopfschmerzen
+        CL_Bewertetes_Symptom lo_kopfschmerzen_8 = new CL_Bewertetes_Symptom(lo_symptom_3, 8);
+        
+        //Halsschmerzen
+        CL_Bewertetes_Symptom lo_halsschmerzen_7 = new CL_Bewertetes_Symptom(lo_symptom_4, 7);
+
+        //Grippe
+        CL_Krankheit lo_erkaeltung = new CL_Krankheit("Hier kommt die Beschreibung", lo_empfhelung_1, 54, "Erkältung");
+        lo_erkaeltung.addSymptom(lo_husten_9);
+        lo_erkaeltung.addSymptom(lo_kopfschmerzen_8);
+        lo_erkaeltung.addSymptom(lo_halsschmerzen_7);
+               
+        //Gewählte Symptome
+
+        ArrayList<CL_Bewertetes_Symptom> lo_gewaehlte_symptome = new ArrayList<>();
+
+        lo_gewaehlte_symptome.add(lo_kopfschmerzen_8);
+
+        CL_Krankheit_Akt_Wert lo_akt_wert_krankheit = new CL_Krankheit_Akt_Wert(lo_gewaehlte_symptome, lo_erkaeltung);
+      
+        CL_Krankheit_Akt_Wert instance = lo_akt_wert_krankheit;
+        
+        double expResult = 0.33;
+        
+        double result = instance.im_cal_akt_wert(lo_gewaehlte_symptome);
+        
+        assertEquals(expResult, result, 0.1);
+       
+    }
+    
+    /**
+     * Test of im_cal_akt_wert method, of class CL_Krankheit_Akt_Wert.
+     * Test auf kein getroffenes Symptom
+     */
+    @Test
+    public void testIm_cal_akt_wert_3() {
+        System.out.println("im_cal_akt_wert");
+        
+        
+        //Erstellen der benötigten Objekte
+        
+        //Symptome
+        CL_Symptom lo_symptom_1 = new CL_Symptom("Husten");
+        CL_Symptom lo_symptom_2 = new CL_Symptom("Fieber");
+        CL_Symptom lo_symptom_3 = new CL_Symptom("Kopfschmerzen");
+        CL_Symptom lo_symptom_4 = new CL_Symptom("Halsschmerzen");
+        CL_Symptom lo_symptom_7 = new CL_Symptom("laufende Nase");
+        CL_Symptom lo_symptom_8 = new CL_Symptom("Gliederschmerzen");
+
+        //Empfehlung
+        CL_Empfehlung lo_empfhelung_1 = new CL_Empfehlung("Gehen Sie zum Arzt!");
+        
+        //Bewertete Symptome
+        //Husten
+        CL_Bewertetes_Symptom lo_husten_9 = new CL_Bewertetes_Symptom(lo_symptom_1, 9);
+
+        //Kopfschmerzen
+        CL_Bewertetes_Symptom lo_kopfschmerzen_8 = new CL_Bewertetes_Symptom(lo_symptom_3, 8);
+        
+        //Halsschmerzen
+        CL_Bewertetes_Symptom lo_halsschmerzen_7 = new CL_Bewertetes_Symptom(lo_symptom_4, 7);
+
+        //Grippe
+        CL_Krankheit lo_grippe = new CL_Krankheit("Hier kommt die Beschreibung", lo_empfhelung_1, 120, "Erkältung");
+        lo_grippe.addSymptom(lo_husten_9);
+        lo_grippe.addSymptom(lo_kopfschmerzen_8);
+        lo_grippe.addSymptom(lo_halsschmerzen_7);
+                
+        //Gewählte Symptome
+
+        ArrayList<CL_Bewertetes_Symptom> lo_gewaehlte_symptome = new ArrayList<>();
+
+        CL_Krankheit_Akt_Wert lo_akt_wert_krankheit = new CL_Krankheit_Akt_Wert(lo_gewaehlte_symptome, lo_grippe);
+      
+        CL_Krankheit_Akt_Wert instance = lo_akt_wert_krankheit;
+        
+        double expResult = 0.0;
+        
+        double result = instance.im_cal_akt_wert(lo_gewaehlte_symptome);
+        
+        assertEquals(expResult, result, 0.0);
+       
+    }
+    
+     /**
+     * Test of im_cal_akt_wert method, of class CL_Krankheit_Akt_Wert.
+     * Test auf mehr ausgewählte Symptome als die Krankheit besitzt
+     */
+    @Test
+    public void testIm_cal_akt_wert_4() {
+        System.out.println("im_cal_akt_wert");
+                
+        //Erstellen der benötigten Objekte
+        
+        //Symptome
+        CL_Symptom lo_symptom_1 = new CL_Symptom("Husten");
+        CL_Symptom lo_symptom_2 = new CL_Symptom("Fieber");
+        CL_Symptom lo_symptom_3 = new CL_Symptom("Kopfschmerzen");
+        CL_Symptom lo_symptom_4 = new CL_Symptom("Halsschmerzen");
+        CL_Symptom lo_symptom_7 = new CL_Symptom("laufende Nase");
+        CL_Symptom lo_symptom_8 = new CL_Symptom("Gliederschmerzen");
+
+        //Empfehlung
+        CL_Empfehlung lo_empfhelung_1 = new CL_Empfehlung("Gehen Sie zum Arzt!");
+        
+        //Bewertete Symptome
+        //Husten
+        CL_Bewertetes_Symptom lo_husten_9 = new CL_Bewertetes_Symptom(lo_symptom_1, 9);
+
+        //Kopfschmerzen
+        CL_Bewertetes_Symptom lo_kopfschmerzen_8 = new CL_Bewertetes_Symptom(lo_symptom_3, 8);
+        
+        //Halsschmerzen
+        CL_Bewertetes_Symptom lo_halsschmerzen_7 = new CL_Bewertetes_Symptom(lo_symptom_4, 7);
+
+        //Grippe
+        CL_Krankheit lo_grippe = new CL_Krankheit("Hier kommt die Beschreibung", lo_empfhelung_1, 120, "leichte Erkältung");
+        lo_grippe.addSymptom(lo_kopfschmerzen_8);
+        lo_grippe.addSymptom(lo_halsschmerzen_7);
+                
+        //Gewählte Symptome
+
+        ArrayList<CL_Bewertetes_Symptom> lo_gewaehlte_symptome = new ArrayList<>();
+
+        lo_gewaehlte_symptome.add(lo_husten_9);
+        lo_gewaehlte_symptome.add(lo_kopfschmerzen_8);
+        lo_gewaehlte_symptome.add(lo_halsschmerzen_7);
+
+        CL_Krankheit_Akt_Wert lo_akt_wert_krankheit = new CL_Krankheit_Akt_Wert(lo_gewaehlte_symptome, lo_grippe);
+      
+        CL_Krankheit_Akt_Wert instance = lo_akt_wert_krankheit;
+        
+        double expResult = 0.45;
+        
+        double result = instance.im_cal_akt_wert(lo_gewaehlte_symptome);
+        
+        assertEquals(expResult, result, 0.0);
+       
+    }
+    
+    
+
 }
